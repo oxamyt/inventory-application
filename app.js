cbf6d93b1ad3029fd015e75dd8c10074ddf4898c
oxamyt <dentez.danil@gmail.com>
2024-08-18 14:35:53 +0300

    setup css

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
